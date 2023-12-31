b456b434b25fb65f92a93d460278060f666338ad
masai <masai@DESKTOP-NI5P0F3.mshome.net>
2023-12-31 14:17:10 +0900

    jh_config 지영취합, 로그인 기능 생성중

---
+++ saerok/src/main/java/com/saerok/jh/common/mapper/EmployeeMapper.java
@@ -0,0 +1,39 @@
+package com.saerok.jh.common.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.saerok.jh.employee.model.dto.Employee;
+
+@Mapper // Mapper 기능을 하는 인터페이스
+public interface EmployeeMapper {
+
+	//사원전체조회
+	@Select("SELECT * FROM EMPLOYEE")
+	List<Employee> selectEmployeeAll();
+	
+	//로그인 
+	@Select("SELECT * FROM EMPLOYEE WHERE EMP_NO=#{empNo}")
+	Employee selectEmployeeByEmpNo(String empNo);
+
+	//사원가입(추가)
+	@Insert("INSERT INTO EMPLOYEE" + "VALUES(#{empName},#{empPw},#{empNo},"
+			+ "#{empPhone},#{empEmail},#{deptCode},#{empAddr},#{empDetailAddr},#{jobCode},#{empDate}")
+	int insertEmployee(Employee e);
+
+	@Update("UPDATE EMPLOYEE SET\r\n"
+			+ "		EMP_NAME=#{empName}, EMP_PW=#{password}, GENDER=#{gender}, AGE=#{age},"
+			+ "		EMAIL=#{email}, PHONE=#{phone}, ADDRESS=#{address} WHERE\r\n"
+			+ "		USERID=#{userId}")
+	int updateMember(Employee m);
+
+	@Delete("DELETE FROM MEMBER"
+			+ "		WHERE USERID=#{userId}")
+	int deleteMember(String userId);
+
+}
--- saerok/src/main/java/com/saerok/jh/security/DBConnectionProvider.java
+++ saerok/src/main/java/com/saerok/jh/security/DBConnectionProvider.java
@@ -1,6 +1,9 @@
 //package com.saerok.jh.security;
 //
 //import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.stereotype.Component;
 //
@@ -17,11 +20,25 @@
 //	//비밀번호 암호화 처리 
 //	private BCryptPasswordEncoder encoder =new BCryptPasswordEncoder();
 //	
+//	//인증하는 메소드 
 //	@Override 
-//	pu
+//	public Authentication authenticate(Authentication authentication) throws AuthenticationException{
+//		String empNo=authentication.getName(); //아이디를 가져옴
+//		String empPw=(String)authentication.getCredentials(); //비밀번호 가져옴
+//		
+//		
+//		
+//	}
 //	
 //	
 //	
+//	
+//	@Override
+//	public boolean supports(Class<?> authentication) { //어떤 인증을 지원 할 것이냐 하는 메소드  //토큰 타입과 일치하면 인증
+//		// TODO Auto-generated method stub
+//		return UsernamePasswordAuthenticationToken
+//				.class.isAssignableFrom(authentication);
+//	}
 //	
 //	
 //	
