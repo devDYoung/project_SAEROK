8136de88ec3592ed66628a5ae889d8fbed89eb43
devjh502 <qmfhak@gmail.com>
2024-01-05 16:33:10 +0900

    jh_마이페이지 수정후 로그아웃 안거치고 바로 수정됨

--- saerok/src/main/java/com/saerok/jh/login/model/service/LoginServiceImpl.java
+++ saerok/src/main/java/com/saerok/jh/login/model/service/LoginServiceImpl.java
@@ -1,10 +1,14 @@
 package com.saerok.jh.login.model.service;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.saerok.jh.employee.model.dto.Employee;
 import com.saerok.jh.login.model.dao.LoginDao;
+import com.saerok.jh.security.DBConnectionProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,8 +19,8 @@ public class LoginServiceImpl implements LoginService {
 
 	private final LoginDao dao;
 	private final SqlSession session;
+	//private final DBConnectionProvider authManager;
 	//private final BCryptPasswordEncoder encoder;
-	
 	
 	//로그인
 	@Override
@@ -29,7 +33,16 @@ public class LoginServiceImpl implements LoginService {
 	//마이페이지
 	
 	public int updateMyPage(Employee e) {
-		return dao.updateMyPage(session,e);
+		int result=dao.updateMyPage(session, e);
+		if(result>0) {
+			Employee loginEmployee=dao.selectEmployeeByEmpNo(session, e.getEmpNo());
+			DBConnectionProvider authManager=new DBConnectionProvider(this);
+			Authentication auth=authManager.authenticate(new UsernamePasswordAuthenticationToken(loginEmployee, 
+					"updateData",
+				loginEmployee.getAuthorities()));
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+		return result;
 	}
 	
 
