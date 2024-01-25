7dfcfdebbad62c9d5ee81eb585ad9cd2abcceafb
yychdfdf <coghk66811@naver.com>
2024-01-25 12:35:55 +0900

    ch 수정

--- saerok/src/main/java/com/saerok/ch/owner/order/controller/OwnerOrderController.java
+++ saerok/src/main/java/com/saerok/ch/owner/order/controller/OwnerOrderController.java
@@ -3,12 +3,15 @@ package com.saerok.ch.owner.order.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.saerok.ch.order.model.dto.OrderList;
@@ -51,6 +54,8 @@ public class OwnerOrderController {
         return "order/orderOwnerDetails"; 
     }
     
+    
+   
 	
 
 	
--- saerok/src/main/webapp/WEB-INF/views/order/orderOwnerDetails.jsp
+++ saerok/src/main/webapp/WEB-INF/views/order/orderOwnerDetails.jsp
@@ -152,9 +152,10 @@
             <button id="cancelOrderButton" class="custom-btn btn-reject">발주 취소하기</button>
         </div>
     </c:if>
-
-
 	</div>
+	
+	
+</script>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script
 		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
